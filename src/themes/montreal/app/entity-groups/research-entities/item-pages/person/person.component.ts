95fe300b931d251a8c35a8a477eb6c82b870f45a
Natalia Jabinschi <natalia.jabinschi@umontreal.ca>
2026-09-14 16:31:09 -0400

    Complète la fiche Person (montreal) ORCID cliquable, discipline, URI, relations Affiliation/Publications/ Direction de recherche ; retrait du prénom/nom et du préfixe de titre ; nouvelles clés i18n préfixées papyrus.

--- src/themes/montreal/app/entity-groups/research-entities/item-pages/person/person.component.ts
+++ src/themes/montreal/app/entity-groups/research-entities/item-pages/person/person.component.ts
@@ -7,8 +7,9 @@ import { Context } from '../../../../../../../app/core/shared/context.model';
 import { ViewMode } from '../../../../../../../app/core/shared/view-mode.model';
 import { PersonComponent as BaseComponent } from '../../../../../../../app/entity-groups/research-entities/item-pages/person/person.component';
 import { GenericItemPageFieldComponent } from '../../../../../../../app/item-page/simple/field-components/specific-field/generic/generic-item-page-field.component';
+import { ItemPageOrcidFieldComponent } from '../../../../../../../app/item-page/simple/field-components/specific-field/orcid/item-page-orcid-field.component';
 import { ThemedItemPageTitleFieldComponent } from '../../../../../../../app/item-page/simple/field-components/specific-field/title/themed-item-page-field.component';
-import { TabbedRelatedEntitiesSearchComponent } from '../../../../../../../app/item-page/simple/related-entities/tabbed-related-entities-search/tabbed-related-entities-search.component';
+import { ItemPageUriFieldComponent } from '../../../../../../../app/item-page/simple/field-components/specific-field/uri/item-page-uri-field.component';
 import { RelatedItemsComponent } from '../../../../../../../app/item-page/simple/related-items/related-items-component';
 import { listableObjectComponent } from '../../../../../../../app/shared/object-collection/shared/listable-object/listable-object.decorator';
 import { DsoEditMenuComponent } from '../../../../../../../app/shared/dso-page/dso-edit-menu/dso-edit-menu.component';
@@ -38,8 +39,9 @@ import { ThemedThumbnailComponent } from '../../../../../../../app/thumbnail/the
     ThemedThumbnailComponent,
     // DSpace item-page
     GenericItemPageFieldComponent,
+    ItemPageOrcidFieldComponent,
+    ItemPageUriFieldComponent,
     RelatedItemsComponent,
-    TabbedRelatedEntitiesSearchComponent,
   ],
 })
 export class PersonComponent extends BaseComponent {}
